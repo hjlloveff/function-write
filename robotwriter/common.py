43ed459ca8dd33eb180a772c7bdd551d29164711
mikechiu <mikechiu@emotibot.com>
2017-05-19 11:50:46 +0800

    Add [IN] and [OUT] of get api be called.

--- robotwriter/common.py
+++ robotwriter/common.py
@@ -9,7 +9,13 @@ def time_calc_decorator(datadog_statsd=None):
     def real_time_calc_decorator(func):
         def wrapper(obj, *args, **kwargs):
             _logger = getattr(obj, 'logger', logger)
-            _logger.info('[IN] %s:%s', obj.__class__.__name__, func.__name__)
+            in_prefix = ''
+            out_prefix = ''
+            if datadog_statsd:
+                in_prefix = u'[IN] '
+                out_prefix = u'[OUT] '
+            _logger.info('%s%s:%s', in_prefix, obj.__class__.__name__,
+                         func.__name__)
             t_p = time.time()
             try:
                 if datadog_statsd:
@@ -18,8 +24,9 @@ def time_calc_decorator(datadog_statsd=None):
                         return func(obj, *args, **kwargs)
                 return func(obj, *args, **kwargs)
             finally:
-                _logger.info('[OUT] %s:%s(%s, %s): cost: %s',
-                             obj.__class__.__name__, func.__name__, args,
+                _logger.info('%s%s:%s(%s, %s): cost: %s',
+                             out_prefix, obj.__class__.__name__,
+                             func.__name__, args,
                              kwargs, time.time() - t_p)
         return wrapper
     return real_time_calc_decorator
