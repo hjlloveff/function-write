3904f77fcc95d7376b606896ec30e7e92f669e49
mikechiu <mikechiu@emotibot.com>
2017-08-21 14:18:01 +0800

    1) add cache with maximum entries 1000 2) fix sky none will cause return 500 issue

--- cache.py
+++ cache.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import time
+from collections import OrderedDict
 
 
 class DictCache(object):
-    def __init__(self, expiration=30*60):
+    def __init__(self, expiration=30*60, size=1000):
+        assert isinstance(expiration, int)
+        assert isinstance(size, int) and size > 0
+
         self.expiration = expiration
-        self.cache = dict()
+        self.cache = OrderedDict()
+        self.size = size
+
+    def __str__(self):
+        return u'\n'.join('%s: %s' % (k, v) for k, v in vars(self).iteritems())
+
+    def __len__(self):
+        return len(self.cache)
 
     def get(self, k):
         v = self.cache.get(k)
@@ -21,10 +32,22 @@ class DictCache(object):
 
         self.cache[k] = v_
 
+        # deal cache full case
+        if len(self.cache) > self.size:
+            try:
+                # remove first insert item
+                self.cache.popitem(last=False)
+            except:
+                pass
+
 
 if __name__ == '__main__':
-    cache = DictCache(expiration=5)
-    cache.update('a', 5)
-    print cache.get('a')
-    time.sleep(5)
-    print cache.get('a')
+    cache = DictCache(expiration=5, size=5)
+    cache.update('1', u'上海')
+    cache.update('2', 'b')
+    cache.update('3', u'北京')
+    cache.update('4', None)
+    cache.update('5', 'e')
+    cache.update('6', 'f')
+    assert len(cache) == 5
+    print cache
