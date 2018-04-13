62673228ef8580199eb656796e20850d64809212
changminyang <changminyang@emotibot.com>
2018-04-13 15:49:36 +0800

    docker suse porting

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=robotwriter
 DATE=`date +%Y%m%d`
-TAG=$1
+TAG=
 GIT_HASH=$(git rev-parse --short HEAD)
 if [ "$GIT_HASH" == "" ]; then
     GIT_HASH="001"
@@ -15,8 +15,14 @@ DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILDROOT=$DIR/..
 
+DOCKERFILE=$DIR/Dockerfile
+PLATFORM=$1
+if [[ $PLATFORM == "suse" ]] || [[ $PLATFORM == "SUSE" ]]; then
+  DOCKERFILE=$DOCKERFILE"-SUSE"
+  DOCKER_IMAGE=$DOCKER_IMAGE"-SUSE"
+fi
 
 # Build docker
-cmd="docker build -t $DOCKER_IMAGE -f $DIR/Dockerfile $BUILDROOT"
+cmd="docker build -t $DOCKER_IMAGE -f $DOCKERFILE $BUILDROOT"
 echo $cmd
 eval $cmd
